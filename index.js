c2787e0c5c48943824fcb95f80a6b09aedd49e96
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-08-10 02:59:34 -0400

    Finalized, 13/13 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = () => drivers.slice(2, 4);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function fare(multiplySomeValue) {
+    return function finalFare(someValue) {
+        return multiplySomeValue * someValue;
+    };
+}
+
+function fareDoubler (fare) {
+    return fare * 2;
+}
+
+function fareTripler (fare) {
+    return fare * 3;
+}
+
+const selectDifferentDrivers = function(drivers, differentDrivers) {
+    return differentDrivers(drivers);
+}
